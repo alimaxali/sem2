e44bfa081c7ee243e354bce822962e82cb4f8d86
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-01-25 14:53:44 -0800

    Add files via upload

---
+++ sem.sh
@@ -0,0 +1,111 @@
+clear
+
+#colors
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+n=install
+
+
+
+
+cd $HOME/payload5
+
+chat(){
+echo 'def kk(t):' > .sssss.py
+echo '   import sys, time' >> .sssss.py
+echo '   for txt in t + "\n":' >> .sssss.py
+echo '        sys.stdout.write(txt)' >> .sssss.py
+echo '        sys.stdout.flush()' >> .sssss.py
+echo '        time.sleep(9. / 150)' >> .sssss.py
+
+
+echo 'h = "              \033[1;33m[\033[1;32m*\033[1;33m] you welcome in {\033[1;36mpauload5\033[1;33m} py [\033[1;32mali.max\033[1;33m] "' >> .sssss.py
+echo 'kk(h)' >> .sssss.py
+python2 .sssss.py
+
+}
+chat2(){
+echo 'def kk(t):' > .ssss.py
+echo '   import sys, time' >> .ssss.py
+echo '   for txt in t + "\n":' >> .ssss.py
+echo '        sys.stdout.write(txt)' >> .ssss.py
+echo '        sys.stdout.flush()' >> .ssss.py
+echo '        time.sleep(9. / 240)' >> .ssss.py
+
+
+
+echo 'f = "    \033[1;32m [\033[1;33m30\033[1;32m] Continue my account {\033[1;34mFacebook\033[1;32m}"' >> .ssss.py
+echo 'y = "     \033[1;32m[\033[1;33m40\033[1;32m] Follow my channel {\033[1;31mYouTube\033[1;32m}"' >> .ssss.py
+echo 'kk(f)' >> .ssss.py
+echo 'kk(y)' >> .ssss.py
+python2 .ssss.py
+
+}
+
+
+
+echo -e "$green "
+figlet  -f big "           payload 5  "
+chat
+echo -e "$blue"
+echo -e '        .e$$$$e.'$cyan '   Welcome to----->{'$green'2019'$cyan'}'$blue
+sleep 0.1
+echo '      e$$$$$$$$$$e'"                         &&&&&&&&&&&&&"
+sleep 0.1
+echo -e '     $$$$$$$$$$$$$$'" Eslam Nassar""           & $green ali.max $blue &"
+sleep 0.1
+echo '    d$$$$$$$$$$$$$$b'"                       &&&&&&&&&&&&&"
+echo '    $$$$$$$$$$$$$$$$                    '
+sleep 0.1
+echo -e '   4$$$$$$$$$$$$$$$$F'
+sleep 0.1
+echo -e '   4$$$$$$$$$$$$$$$$F'
+sleep 0.1
+echo -e '    $$$" "$$$$" "$$$'
+echo  '    $$F   4$$F   4$$===============||'
+sleep 0.1
+echo -e '     $F '$green"&" $blue'4$$F '$green"&"$blue  '4$                ||       '
+echo '     $$   $$$$   $$                ||      '
+sleep 0.1
+echo '     $$$$$$""$$$$$$                ||     '
+echo -e '      $$$F'$green "o"$blue '4$$$$                 || '
+sleep 0.1
+echo '       "$$$ee$$$"                  ||'
+echo '        4*$$$$F4                \  ||  /'
+sleep 0.1
+echo -e '        $ '$green"----"$blue'.$                 \ || /'
+echo -e '        "$$$$$$"   '$cyan'  [90]helpℹ️ '$blue'   \||/   '
+echo '          $$$$                     \/       '
+sleep 0.1
+echo -e "               $bluo p $green a $reset y $purple l $cyan o $yellow a $green d"
+#echo -e "$red"
+#echo '                               exit=CTRL+c'
+chat2
+echo -e "$green+________________________________________________________+"
+echo -e "$cyan|  [1] metasploit ⚔️|  $blue  [2] nmap 🌎| $purple  [3] dos attack ⛔ |             "
+#echo -e "| $green            --------------------------------            |  "
+echo -e "$yellow|  [4]   ngrok 🌐  |  $red  [5] virus ☢️️|  $reset [6]   Termux 📟   |    "
+echo "                                        "
+echo -e " $purple       |   [7]facebook             [8]Encrypt    |"
+echo -e " $green          --------------------------------------"
+echo -e "                     $red [$cyan"$cyan"7777$red]Dump area"
+
+echo -e " $red "
+echo -e " [00] Exit           $green                          [99] update"
+#echo -e "$green"
+echo -e "$cyan"
+echo -e "|---{$yellow my ip$cyan }---| "
+       curl ifconfig.me
+echo -e "$blue"
+ifconfig wlan0 | grep -o 192..........
+#echo -e "$green "
+
+
