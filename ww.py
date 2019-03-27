f07ebc54c319716bf524592e4d17e1ba2f61d1a0
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-03-27 18:46:15 +0200

    Update ww.py

--- ww.py
+++ ww.py
@@ -1,11 +1,13 @@
 import random
 import os
-e = str(random.randint(1,2))
+e = str(random.randint(1,3))
 
 
 if e in "1":
    os.system("bash sem1.sh")
 elif e in "2":
    os.system("bash sem2.sh")
+elif e in "3":
+   os.system("bash sem3.sh")
 else :
    print "error"
