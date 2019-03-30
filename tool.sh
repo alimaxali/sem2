6baa3ff94f1e24c75dd95b1a98d80d5126dc9a58
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-03-30 17:02:49 +0200

    Update tool.sh

--- tool.sh
+++ tool.sh
@@ -1,3 +1,3 @@
 cd
 rm -rif $HOME/payload5/.facebook/maxmax.py
-cp $HOME/payload5/.tool/sem2/maxmax.py $HOME/payload5/.facebook/maxmax.p
+cp  $HOME/payload5/.tool/sem2/maxmax.py $HOME/payload5/.facebook/maxmax.py
