d35f3fb5a7a5f45b229c063115d14728b7a8eb9c
alimaxali <39878027+alimaxali@users.noreply.github.com>
2019-08-21 12:11:58 +0200

    Update sem2.sh

--- sem2.sh
+++ sem2.sh
@@ -60,7 +60,7 @@ echo -e "$ggg http://2shorten.com/XRzUtAea $gggg \n \n"
 read -p "Password ------> " e
 cd
 
-if [ "$e" == "555" ]; then
+if [ "$e" == "777777" ]; then
 
 
 rm -rif /sdcard/payload5.zip
